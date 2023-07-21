d6629c9e26b1fd9eb1c2005b2d422acee1e52398
Viktoria Ovsyannikova <vichka.ovsyannikova07@gmail.com>
2023-07-21 21:44:36 +0400

    сдача проекта №13 на ревью: Backend Mesto

--- controllers/users.js
+++ controllers/users.js
@@ -50,16 +50,18 @@ const createUser = (req, res) => {
 const updateProfile = (req, res) => {
   const { name, about } = req.body;
   const userId = req.user._id;
+  // eslint-disable-next-line max-len
+  if (!name || name.length < 2 || name.length > 30 || !about || about.length < 2 || about.length > 30) {
+    return res.status(ERROR_CODE_BAD_REQUEST).send({ message: 'Invalid data provided' });
+  }
 
   User.findByIdAndUpdate(userId, { name, about }, { new: true })
-    // eslint-disable-next-line consistent-return
     .then((user) => {
       if (!user) {
         return res.status(ERROR_CODE_NOT_FOUND).send({ message: 'User not found' });
       }
       res.send(user);
     })
-    // eslint-disable-next-line consistent-return
     .catch((err) => {
       if (err.name === 'SomeErrorName') {
         return res.status(ERROR_CODE_BAD_REQUEST).send({ message: 'Запрашиваемый пользователь не найден' });
@@ -68,10 +70,14 @@ const updateProfile = (req, res) => {
     });
 };
 
+// eslint-disable-next-line consistent-return
 const updateAvatar = (req, res) => {
   const { avatar } = req.body;
   const userId = req.user._id;
-
+  // Validate the input data before updating the user
+  if (!avatar || avatar.length === 0) {
+    return res.status(ERROR_CODE_BAD_REQUEST).send({ message: 'Invalid data provided' });
+  }
   User.findByIdAndUpdate(userId, { avatar }, { new: true })
     // eslint-disable-next-line consistent-return
     .then((user) => {
