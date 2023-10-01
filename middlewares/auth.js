628eac7dbeb7e6613522b0ca354a7b20715f4b1d
Viktoria Ovsyannikova <vichka.ovsyannikova07@gmail.com>
2023-10-01 18:09:09 +0300

    проверка теста 14

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,19 +2,19 @@ const jwt = require('jsonwebtoken');
 const ValidationError = require('../errors/ValidationError');
 
 const extractBearerToken = (header) => header.replace('Bearer ', '');
+
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new ValidationError('Необходима авторизация!'));
+    return next();
   }
   const token = extractBearerToken(authorization);
-  let payload;
   try {
-    payload = jwt.verify(token, 'super-strong-secret');
+    const payload = jwt.verify(token, 'super-strong-secret');
+    req.user = payload;
+    next();
   } catch (err) {
     return next(new ValidationError('Неверный токен'));
   }
-  req.user = payload;
-  next();
 };
